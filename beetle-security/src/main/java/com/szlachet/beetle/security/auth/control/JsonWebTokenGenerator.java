2a5406c074c3f85f405792d11569725176eda354
Sebastian Szlachetlka <sszlach@softserveinc.com>
2016-06-03 08:37:00 +0200

    Added JWKs.

--- beetle-security/src/main/java/com/szlachet/beetle/security/auth/control/JsonWebTokenGenerator.java
+++ beetle-security/src/main/java/com/szlachet/beetle/security/auth/control/JsonWebTokenGenerator.java
@@ -3,10 +3,9 @@ package com.szlachet.beetle.security.auth.control;
 import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import org.jose4j.jwk.RsaJsonWebKey;
-import org.jose4j.jwk.RsaJwkGenerator;
 import org.jose4j.jws.AlgorithmIdentifiers;
 import org.jose4j.jws.JsonWebSignature;
 import org.jose4j.jwt.JwtClaims;
@@ -19,24 +18,13 @@ import org.jose4j.lang.JoseException;
 @ApplicationScoped
 public class JsonWebTokenGenerator {
     private static final Logger LOGGER = Logger.getLogger(JsonWebTokenGenerator.class.getName());
-    private RsaJsonWebKey rsaJsonWebKey;
     
-    @PostConstruct
-    private void initRsaJsonWebKey() {
-        try {
-            rsaJsonWebKey = RsaJwkGenerator.generateJwk(2048);
-            rsaJsonWebKey.setKeyId("BeetleKeyId01");
-            LOGGER.log(Level.INFO, "Public key: {0}", rsaJsonWebKey.getPublicKey().getEncoded());
-            LOGGER.log(Level.INFO, "Private key: {0}", rsaJsonWebKey.getPrivateKey().getEncoded());
-            
-        } catch (JoseException ex) {
-            LOGGER.log(Level.SEVERE, "", ex);
-        }
-    }
+    @Inject 
+    private RsaKeyGenerator rsaKeyGenerator;
     
-    //TODO build Json Web Token using jose4j 
     public String generateJsonWebToken() {
-        JwtClaims claims = new JwtClaims();
+        final RsaJsonWebKey rsaJsonWebKey = rsaKeyGenerator.getRsaJsonWebKey();
+        final JwtClaims claims = new JwtClaims();
         claims.setIssuer("beetle");
         claims.setAudience("beetle");
         claims.setExpirationTimeMinutesInTheFuture(5);
@@ -46,11 +34,11 @@ public class JsonWebTokenGenerator {
         claims.setClaim("email", "sebastian.szlachetka@gmail.com");
         claims.setStringListClaim("groups", Arrays.asList("group-one", "group-two"));
         
-        JsonWebSignature jws = new JsonWebSignature();
+        final JsonWebSignature jws = new JsonWebSignature();
         jws.setPayload(claims.toJson());
         jws.setKey(rsaJsonWebKey.getPrivateKey());
         jws.setKeyIdHeaderValue(rsaJsonWebKey.getKeyId());
-        jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.RSA_PSS_USING_SHA256);
+        jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.HMAC_SHA256);
         String jwt = "";
         try {
             jwt = jws.getCompactSerialization();
---
+++ beetle-security/src/main/java/com/szlachet/beetle/security/auth/control/RsaKeyGenerator.java
@@ -0,0 +1,48 @@
+package com.szlachet.beetle.security.auth.control;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import org.jose4j.jwk.JsonWebKeySet;
+import org.jose4j.jwk.RsaJsonWebKey;
+import org.jose4j.jwk.RsaJwkGenerator;
+import org.jose4j.jws.AlgorithmIdentifiers;
+import org.jose4j.lang.JoseException;
+
+/**
+ *
+ * @author Sebastian Szlachetka
+ */
+@ApplicationScoped
+public class RsaKeyGenerator {
+    
+    private static final Logger LOGGER = Logger.getLogger(RsaKeyGenerator.class.getName());
+    private final JsonWebKeySet jsonWebKeySet = new JsonWebKeySet();
+    private RsaJsonWebKey rsaJsonWebKey;
+    
+    @PostConstruct
+    private void initRsaJsonWebKey() {
+        generateRsaJsonWebKey();
+        jsonWebKeySet.addJsonWebKey(rsaJsonWebKey);
+    }
+    
+    public JsonWebKeySet getJsonwebKeySet() {
+        return jsonWebKeySet;
+    }
+    
+    public RsaJsonWebKey getRsaJsonWebKey() {
+        return rsaJsonWebKey;
+    }
+    
+    private void generateRsaJsonWebKey() {
+        try {
+            rsaJsonWebKey = RsaJwkGenerator.generateJwk(2048);
+            rsaJsonWebKey.setKeyId("BeetleKeyId01");
+            rsaJsonWebKey.setAlgorithm(AlgorithmIdentifiers.HMAC_SHA256);
+            LOGGER.log(Level.SEVERE, "RSA JSON Web Key: {0}", rsaJsonWebKey.toJson());
+        } catch (JoseException ex) {
+            LOGGER.log(Level.SEVERE, "RSA key pair could not be generated.", ex);
+        }
+    }
+}
